8fe515546c5fd6539967096603987ddc5acfc573
Vatamaniucdenis88 <vatamaniuc88@gmail.com>
2020-04-17 21:25:26 +0100

    Updates

---
+++ precache-manifest.74fb796cecffdbfbc38b8111478b1222.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3941765be4fc4d50e2224afd8f4d8eb2",
+    "url": "/shoplovers/index.html"
+  },
+  {
+    "revision": "1b588e506af63821af9c",
+    "url": "/shoplovers/static/css/2.e49e09ab.chunk.css"
+  },
+  {
+    "revision": "2b3c75809308408fc6c2",
+    "url": "/shoplovers/static/css/main.a30e6fe2.chunk.css"
+  },
+  {
+    "revision": "1b588e506af63821af9c",
+    "url": "/shoplovers/static/js/2.f45e6087.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/shoplovers/static/js/2.f45e6087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3c75809308408fc6c2",
+    "url": "/shoplovers/static/js/main.ff535555.chunk.js"
+  },
+  {
+    "revision": "7b02021d44842f72de54",
+    "url": "/shoplovers/static/js/runtime-main.76af58b4.js"
+  },
+  {
+    "revision": "0793d4ee1c9b93032311061d5f7c77ae",
+    "url": "/shoplovers/static/media/1.0793d4ee.jpg"
+  },
+  {
+    "revision": "fe4a9118ecd493f4650c14a277127999",
+    "url": "/shoplovers/static/media/10.fe4a9118.jpg"
+  },
+  {
+    "revision": "b19acdbfb7dfaf3ea5e535da18411ffe",
+    "url": "/shoplovers/static/media/4.b19acdbf.jpg"
+  },
+  {
+    "revision": "9640419f876954e7db714c34b0ed41ee",
+    "url": "/shoplovers/static/media/5.9640419f.jpg"
+  },
+  {
+    "revision": "a19d30a3a1a5d1a325308f9aeed21ed7",
+    "url": "/shoplovers/static/media/6.a19d30a3.jpg"
+  },
+  {
+    "revision": "1f0e62da382a132a05eb9f3f7fed521e",
+    "url": "/shoplovers/static/media/7.1f0e62da.jpg"
+  },
+  {
+    "revision": "9b455bd69e2544192ee79384754c23a0",
+    "url": "/shoplovers/static/media/8.9b455bd6.jpg"
+  },
+  {
+    "revision": "823a451661f4cced80b76d899f2c5d7a",
+    "url": "/shoplovers/static/media/9.823a4516.jpg"
+  }
+]);
